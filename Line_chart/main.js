4999a4314130bab549ef56a6a0bba1ae882c04ff
AntoinouM <antoine.muneret@hotmail.fr>
2023-04-06 18:20:03 +0200

    init : data correction

--- Line_chart/main.js
+++ Line_chart/main.js
@@ -4,10 +4,34 @@ import './style.css';
   /* ===== Preparation ===== */
 
 async function drawChart() {
-
+  const width = 700;
+  const height = 500;
+  const circleRadius = 4;
+  const color = '#FFCAE9';
   /* ===== Load Data ===== */
   const dataSet = await d3.json('./data/weather_data_2021.json')
-  console.log(dataSet); 
+    // add a corresponding season to all my entries
+  let dataWithSeason = dataSet.map(function(d) {
+    let month = new Date(d.date).getMonth();
+      d.season = getSeason(month);
+    return d;
+  });
+
+  /* ===== CHART DIMENSION ===== */
+  const margin = {top: 20, right: 20, bottom: 35, left: 35};
+  const boundedWith = width - margin.left - margin.right;
+  const boundedHeight = height - margin.top - margin.bottom;
+
+  /* ===== DRAW CANVAS ===== */
+  const svg = d3.select('#viz')
+    .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+
+  const viz = svg.append('g')
+      .attr('class', 'box-plot')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
 
   // Table view in console
   console.table(dataSet[0]);
@@ -16,8 +40,43 @@ async function drawChart() {
   const yAccessor = d => d.temperatureMax;
   const xAccessore = d => d.date;
 
+
+    
+  console.table(dataWithSeason[0])
+  console.log()
+
+  // compute statistics
+  console.log('===== Stats =====')
+  
+  console.log('=================')
   
 
 }
 
 drawChart();
+
+
+
+// Helpeing methods
+function getSeason(month) {
+  switch (month) {
+    case 11:
+    case 0:
+    case 1:
+      return "Winter";
+    case 2:
+    case 3:
+    case 4:
+      return "Spring";
+    case 5:
+    case 6:
+    case 7:
+      return "Summer";
+    case 8:
+    case 9:
+    case 10:
+      return "Fall";
+    default:
+      return "Unknown";
+  }
+}
