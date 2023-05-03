d9eb15ac3ce96d7c23d4d3104974eb8ecc54e49f
cc211036 <cc211036@hotmail.fr>
2023-05-03 15:37:45 +0200

    add: slider changes

--- map-START/main.js
+++ map-START/main.js
@@ -1,12 +1,13 @@
 import * as d3 from 'd3';
 import { transformData } from './src/util';
+
 import './style.css';
 
-
-
-const width = window.innerWidth * 0.9
-const height = window.innerHeight * 0.6 //60% screen width
-
+// ====================
+// SETUP
+// ====================
+const width = window.innerWidth * 0.8;
+const height = window.innerHeight * 0.7;
 
 const minYear = 1913;
 const maxYear = 2019;
@@ -14,97 +15,158 @@ let currentYear = 2000;
 let minLifeE, meanLifeE, maxLifeE;
 let mergedGeoJson;
 
-// reference to the tooltip
+// Reference to the tooltip
 const tooltip = d3.select('#tooltip');
 const tooltipText = d3.select('#information');
 
-// reference to the current year
+// Reference to current year
 const currentYearEl = d3.select('#currentYear');
-//Generators
-const projection = d3.geoMercator()
-  .translate([width / 2, height / 1.4])
-  .scale([150])
 
+// Generators
+const projection = d3
+  .geoMercator()
+  .translate([width / 2, height / 1.4]) // Translate to center screen
+  .scale([150]);
 const path = d3.geoPath().projection(projection);
+
+// Scales
 const colorScale = d3
   .scaleSequential()
-  .interpolator(d3.interpolateRdYlGn)
+  .interpolator(d3.interpolateRdYlGn);
 
+// Linear scale with pivot element in order to emphasize extremes
 const colorScale2 = d3
   .scaleLinear()
   .range(['#cb0f20', '#ecf279', '#2f6e5f']);
 
 
 const drawViz = async () => {
+  // ====================
+  // Load Data
+  // ====================
 
-  //load world data
+  // Load world data
   const worldData = await d3.json('./data/world.geo.json');
-  //load life expectancy data
-  const lifeData = await d3.csv('./data/life-expectancy.csv', (row) => { // new feature of d3, that allow to modify data before storing it in the const
+
+  // Load life expactancy data
+  const lifeData = await d3.csv('./data/life-expectancy.csv', (row) => { // allow to modify dataset
     return {
       entity: row['Entity'],
-      lifeExpectacy: +row['LifeExpectacy'],
+      lifeExpectancy: +row['LifeExpectacy'],
       code: row['Code'],
-      year: +row['Year']
-    }
+      year: +row['Year'],
+    };
   });
 
-  minLifeE = Math.ceil(d3.min(lifeData, (d) => d.lifeExpectacy));
-  meanLifeE = Math.ceil(d3.mean(lifeData, (d) => d.lifeExpectacy));
-  maxLifeE = Math.ceil(d3.max(lifeData, (d) => d.lifeExpectacy));
+  // Get the min, mean and max life expactancy from the data
+  minLifeE = Math.ceil(d3.min(lifeData, (d) => d.lifeExpectancy));
+  meanLifeE = Math.ceil(d3.mean(lifeData, (d) => d.lifeExpectancy));
+  maxLifeE = Math.ceil(d3.max(lifeData, (d) => d.lifeExpectancy));
 
+  // Initial call with the default year
   mergedGeoJson = transformData(lifeData, worldData, currentYear);
 
-  // =====================
-  // Generators and Scales
-  // =====================
+  // ====================
+  // Scales
+  // ====================
 
-
-  //Scales
-
-  //Sequential scale to show the extremes with diverging colors
+  // Sequential scale to show the extremes with diverging colors
   colorScale.domain([minLifeE, maxLifeE]);
 
-  // Linerar scale
+  // Linear Scale
   colorScale2.domain([minLifeE, meanLifeE, maxLifeE]);
 
-
-  // =====================
-  // Draw a map
-  // =====================
-  const svg = d3.select('#viz')
+  // ====================
+  // DRAW
+  // ====================
+  const svg = d3
+    .select('#viz')
     .append('svg')
     .attr('width', width)
-    .attr('height', height)
+    .attr('height', height);
 
   const g = svg.append('g').attr('class', 'mapViz');
+
+  // Draw the countries initially
   drawMap(mergedGeoJson, g);
 
-  // ZOOM AND PAN
+  // ====================
+  // ZOOM & PAN
+  // ====================
+  const zoom = d3.zoom()
+    .scaleExtent([1, 10]) // scale factor
+    .translateExtent([
+      [-300, -300], // x0 and y0
+      [1500, 1000] // x1 and y1
+    ])
+    .on('zoom', function(event) {
+      console.log(event)
+      g.attr('transform', event.transform)
+    });
 
-}
+    // Call the zoom on the next parent element of your 'to be zoomed' selection
+    d3.select('#viz').call(zoom);
 
+  // ====================
+  // SLider setup
+  // ====================
+  const slider = d3.select('#currentYearSlider').on('input', function() {
+    // Update the current year globally
+    currentYear = +this.value; // + there convert to a number
+
+    // Display
+    currentYearEl.html(currentYear);
+
+    //call the redraw function
+    redraw(currentYear);
+  })
+
+  // ====================
+  // Redraw function
+  // ====================
+  function redraw(currentYear) {
+    // 1. Grab the data based on the current year
+    mergedGeoJson = transformData(lifeData, worldData, currentYear);
+    
+    // 2. Redraw the countries based on the new data
+    drawMap(mergedGeoJson, g);
+  }
+
+};
+
+/**
+ * This function draws the map with the current data set
+ * @param {*} mergedGeoJson 
+ * @param {*} sel 
+ */
 const drawMap = (mergedGeoJson, sel) => {
-  const countries = sel.selectAll('path')
+  const countries = sel
+    .selectAll('path')
     .data(mergedGeoJson)
     .join('path')
     .attr('d', path)
     .attr('class', 'country')
     .style('fill', (d) => {
-      return colorScale2(d.lifeExpectacy)
+      return colorScale2(d.lifeExpectancy)
     })
-    createTooltips(countries);
-}
 
+  // Call tooltlip function with countries
+  createTooltips(countries);
+};
+
+/**
+ * This function creates the tooltips for the current countries based on the data set
+ * @param {*} countries 
+ */
 const createTooltips = (countries) => {
   countries.on('mousemove', function(event, d) {
-    // Move the tooltip itself
-    tooltip
-      .style('opacity', 1)
-      .style('left', event.pageX - 86 + 'px')
-      .style('top', event.pageY - 60 + 'px');
-    tooltipText
-      .html(function() {
+    if (d.hasOwnProperty('year')) {
+      // Move the tooltip itself
+      tooltip.style('opacity', 1)
+        .style('left', event.pageX - 86 + 'px')
+        .style('top', event.pageY - 60 + 'px');
+
+      tooltipText.html(function() {
         return `
           <table>
             <tr>
@@ -113,14 +175,21 @@ const createTooltips = (countries) => {
             </tr>
             <tr>
               <td><b>Life Expactancy:</b></td>
-              <td>${Math.ceil(d.lifeExpectacy)}</td>
-           </tr>
+              <td>${Math.ceil(d.lifeExpectancy)}</td>
+            </tr>
           </table>
         `
-      })
+      });
+    }
   }).on('mouseout', function() {
     tooltip.style('opacity', 0);
-  })
+  });
 }
 
+// Draw the map for the first time
 drawViz();
+
+// ====================
+// Play button
+// ====================
+const playBtn = d3.select('#btnPlay');
